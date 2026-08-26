f27be4aeeb1b0f39addf4a514c6f60d518a7e551
Seaverhall10 <seaverhall@gmail.com>
2026-08-26 10:14:53 -0500

    Serve compact header from docs/

---
+++ docs/firebase-config.js
@@ -0,0 +1,9 @@
+/* PLACEHOLDER: replace with your Firebase web config before deploy. Until then the app uses localStorage. */
+window.FIREBASE_CONFIG = {
+  apiKey: "REPLACE_ME",
+  authDomain: "REPLACE_ME.firebaseapp.com",
+  projectId: "REPLACE_ME",
+  storageBucket: "REPLACE_ME.appspot.com",
+  messagingSenderId: "REPLACE_ME",
+  appId: "REPLACE_ME",
+};
